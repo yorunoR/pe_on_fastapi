4268a94dc06ed0265a6c24ff1fb6a88da56e2210
Yoshioka <sss.yoshioka@gmail.com>
2023-06-22 00:33:17 +0000

    🎸 feat: add CORS support

--- pe_on_fastapi/main.py
+++ pe_on_fastapi/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pe_on_fastapi.routers import executor
 
 app = FastAPI()
+
 app.include_router(executor.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
